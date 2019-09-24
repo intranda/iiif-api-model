cd4ba5b4048a8de893a810488d4d51e75360f021
Florian Alpers <florian.alpers@intranda.com>
2019-09-24 10:37:41 +0200

    Create FragmentSelector from fragment String

--- iiif-api-model/src/main/java/de/intranda/api/annotation/wa/FragmentSelector.java
+++ iiif-api-model/src/main/java/de/intranda/api/annotation/wa/FragmentSelector.java
@@ -1,6 +1,8 @@
 package de.intranda.api.annotation.wa;
 
 import java.awt.Rectangle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,11 +13,25 @@ import de.intranda.api.annotation.ISelector;
 public class FragmentSelector implements ISelector {
 
     private static final String TYPE = "FragmentSelector";
+    private static final String FRAGMENT_REGEX = "xywh=(-?\\d+),(-?\\d+),(-?\\d+),(-?\\d+)";
 
     private final Rectangle fragment;
 
     public FragmentSelector(Rectangle fragment) {
         this.fragment = fragment;
+    }
+    
+    public FragmentSelector(String fragment) {
+        Matcher matcher = Pattern.compile(FRAGMENT_REGEX).matcher(fragment);
+        if(matcher.find()) {
+            int x = Integer.parseInt(matcher.group(1));
+            int y = Integer.parseInt(matcher.group(2));
+            int w = Integer.parseInt(matcher.group(3));
+            int h = Integer.parseInt(matcher.group(4));
+            this.fragment = new Rectangle(x, y, w, h);
+        } else {
+            throw new IllegalArgumentException("Cannot parse '" + fragment + "' as rectangle coordinates");
+        }
     }
     
     
