31d70451542948f3f5dd1cbd10d571214453ae33
Florian Alpers <florian.alpers@intranda.com>
2019-11-08 10:17:25 +0100

    fixed url only serialization error

--- iiif-api-model/src/main/java/de/intranda/api/serializer/URLOnlySerializer.java
+++ iiif-api-model/src/main/java/de/intranda/api/serializer/URLOnlySerializer.java
@@ -81,6 +81,7 @@ public class URLOnlySerializer extends JsonSerializer<Object> {
                 for (Object child : collection) {
                     write(child, generator);
                 }
+                generator.writeEndArray();
             }
         }
     }
