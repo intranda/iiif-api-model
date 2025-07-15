2c8967edca2dfa293bc7a5f29b969d242772fbd2
Andrey Kozhushkov <andrey.kozhushkov@intranda.com>
2025-07-15 18:56:09 +0200

    added services to TypeResource; refs #26497

--- iiif-api-model/src/main/java/de/intranda/api/annotation/wa/TypedResource.java
+++ iiif-api-model/src/main/java/de/intranda/api/annotation/wa/TypedResource.java
@@ -1,6 +1,8 @@
 package de.intranda.api.annotation.wa;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -9,18 +11,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-import de.intranda.api.annotation.IResource;
 import de.intranda.api.annotation.ITypedResource;
 import de.intranda.api.deserializer.ResourceDeserializer;
+import de.intranda.api.services.Service;
 
 @JsonInclude(Include.NON_NULL)
-@JsonDeserialize(using=ResourceDeserializer.class)
+@JsonDeserialize(using = ResourceDeserializer.class)
 public class TypedResource implements ITypedResource {
 
     private final String type;
     private final String format;
     private final URI id;
     private final String profile;
+    private final List<Service> service = new ArrayList<>();
 
     public TypedResource(URI id, String type, String format, String profile) {
         this.id = id;
@@ -28,15 +31,15 @@ public class TypedResource implements ITypedResource {
         this.format = format;
         this.profile = profile;
     }
-    
+
     public TypedResource(URI id, String type, String format) {
         this(id, type, format, null);
     }
 
     public TypedResource(URI id, String type) {
-    	this(id, type, null, null);
+        this(id, type, null, null);
     }
-    
+
     public TypedResource() {
         this(null, null, null, null);
     }
@@ -54,35 +57,32 @@ public class TypedResource implements ITypedResource {
     public String getFormat() {
         return format;
     }
-    
+
     @Override
     public URI getId() {
         return id;
     }
-    
+
     public String getProfile() {
-    	return profile;
+        return profile;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
-        if(obj.getClass().equals(this.getClass())) {
-            TypedResource other = (TypedResource)obj;
-            return this.getId().equals(other.getId()) 
+        if (obj.getClass().equals(this.getClass())) {
+            TypedResource other = (TypedResource) obj;
+            return this.getId().equals(other.getId())
                     && Objects.equals(this.getType(), other.getType())
                     && Objects.equals(this.getFormat(), other.getFormat());
-        } else {
-            return false;
         }
+        return false;
     }
-    
 
     @Override
     public String toString() {
         return asJson();
     }
-    
-    
+
     public String asJson() {
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -91,5 +91,18 @@ public class TypedResource implements ITypedResource {
             return super.toString();
         }
     }
-    
+
+    /**
+     * @return the service
+     */
+    @JsonInclude(Include.NON_EMPTY)
+    public List<Service> getService() {
+        return service;
+    }
+
+    public void addService(Service service) {
+        if (service != null) {
+            this.service.add(service);
+        }
+    }
 }
